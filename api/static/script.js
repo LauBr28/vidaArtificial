cf6220abff083773973970355a5c402b4d36988d
Aleja <laura.barrera04@usa.edu.co>
2025-02-16 17:36:36 +0000

    Interfaz lista

--- api/static/script.js
+++ api/static/script.js
@@ -1,19 +1,37 @@
-document.getElementById('sentimentForm').onsubmit = async function (event) {
+function showForm(model) {
+    // Oculta todos los formularios
+    document.querySelectorAll('.form').forEach(form => {
+        form.classList.add('hidden');
+    });
+
+    // Muestra el formulario seleccionado
+    document.getElementById(`${model}-form`).classList.remove('hidden');
+}
+
+async function submitForm(event, endpoint) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const text = formData.get('text');
+    const data = {};
+
+    formData.forEach((value, key) => {
+        if (key === 'candidate_labels') {
+            data[key] = value.split(',').map(label => label.trim());
+        } else {
+            data[key] = value;
+        }
+    });
 
     try {
-        const response = await fetch('/sentiment-analysis/', {
+        const response = await fetch(endpoint, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ text: text }),
+            body: JSON.stringify(data),
         });
         const result = await response.json();
         document.getElementById('result').innerText = JSON.stringify(result, null, 2);
     } catch (error) {
         document.getElementById('result').innerText = 'Error: ' + error.message;
     }
-};
+}
